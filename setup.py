c1b9a89eb180a1bd56d3f01d6820be47bbf71022
Cesar Rodas <65242055+Care99@users.noreply.github.com>
2024-06-07 16:13:42 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,13 +6,14 @@ MAINTAINER_EMAIL = 'cesar_rodas@outlook.com'
 LICENSE = 'Simplified BSD'
 PACKAGES=['sdtw']
 VERSION = '0.1.dev0'
+CMAKE_INSTALL_DIR='sdtw'
 setup(
-        configuration=configuration,
         name=DISTNAME,
         version=VERSION,
         description=DESCRIPTION,
-        author=MANTAINER,
+        author=MAINTAINER,
         license=LICENSE,
         packages=PACKAGES,
         python_requires=">=3.7",
+        cmake_install_dir=CMAKE_INSTALL_DIR,
     )
