c456d0d00c73c449804f2ac1efb01cfd5ce74ab4
Cesar Rodas <65242055+Care99@users.noreply.github.com>
2024-06-06 21:01:15 -0400

    Update setup.py
    using skbuild.setup()

--- setup.py
+++ setup.py
@@ -1,65 +1,18 @@
-from __future__ import print_function
-import os.path
-import sys
-import setuptools
-from numpy.distutils.core import setup
-
-
-try:
-    import numpy
-except ImportError:
-    print('numpy is required during installation')
-    sys.exit(1)
-
-
+from skbuild import setup
 DISTNAME = 'soft-dtw'
 DESCRIPTION = "Python implementation of soft-DTW"
-LONG_DESCRIPTION = open('README.rst').read()
-MAINTAINER = 'Mathieu Blondel'
-MAINTAINER_EMAIL = ''
-URL = 'https://github.com/mblondel/soft-dtw/'
+MAINTAINER = 'Mathieu Blondel,Cesar Rodas'
+MAINTAINER_EMAIL = 'cesar_rodas@outlook.com'
 LICENSE = 'Simplified BSD'
-DOWNLOAD_URL = 'https://github.com/mblondel/soft-dtw/'
+PACKAGES=['sdtw']
 VERSION = '0.1.dev0'
-
-
-def configuration(parent_package='', top_path=None):
-    from numpy.distutils.misc_util import Configuration
-
-    config = Configuration(None, parent_package, top_path)
-
-    config.add_subpackage('sdtw')
-
-    return config
-
-
-if __name__ == '__main__':
-    old_path = os.getcwd()
-    local_path = os.path.dirname(os.path.abspath(sys.argv[0]))
-
-    os.chdir(local_path)
-    sys.path.insert(0, local_path)
-
-    setup(configuration=configuration,
-          name=DISTNAME,
-          maintainer=MAINTAINER,
-          include_package_data=True,
-          maintainer_email=MAINTAINER_EMAIL,
-          description=DESCRIPTION,
-          license=LICENSE,
-          url=URL,
-          version=VERSION,
-          download_url=DOWNLOAD_URL,
-          long_description=LONG_DESCRIPTION,
-          zip_safe=False,  # the package can run out of an .egg file
-          classifiers=[
-              'Intended Audience :: Science/Research',
-              'Intended Audience :: Developers', 'License :: OSI Approved',
-              'Programming Language :: C', 'Programming Language :: Python',
-              'Topic :: Software Development',
-              'Topic :: Scientific/Engineering',
-              'Operating System :: Microsoft :: Windows',
-              'Operating System :: POSIX', 'Operating System :: Unix',
-              'Operating System :: MacOS'
-             ]
-          )
+setup(
+        configuration=configuration,
+        name=DISTNAME,
+        version=VERSION,
+        description=DESCRIPTION,
+        author=MANTAINER,
+        license=LICENSE,
+        packages=PACKAGES,
+        python_requires=">=3.7",
+    )
